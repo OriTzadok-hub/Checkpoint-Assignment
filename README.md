10d6898f301153a8d5f55c918583082118af7b07
Ori <orizadok5@gmail.com>
2024-02-05 19:03:14 +0200

    add documentation and last edits

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Checkpoint-Assignment
+# DevOps Home Assignment
+
+## Overview
+
+This project automates the deployment of a cloud-based service, using AWS for content delivery via CloudFront and data storage in S3, with exclusive S3 access through CloudFront. It incorporates Terraform and Terragrunt for infrastructure management, GitHub Actions for CI/CD, and a Python script for data processing. This setup demonstrates efficient DevOps practices, focusing on automation, scalability, and secure data access.
+
+## Prerequisites
+
+- AWS Account with access credentials.
+- Local installations of Python, Terraform, and Terragrunt are required only for manual execution and local testing of the infrastructure and application code.
+
+## Setup Instructions
+
+### Automated Deployment with GitHub Actions
+
+1. **AWS Credentials**: Add `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` as secrets in the GitHub repository (`Settings > Secrets`).
+
+2. **Workflow Dispatch**: Trigger the deployment workflow from the `Actions` tab in the repository.
+
+### Manual Deployment
+
+#### Terraform and Terragrunt
+
+1. Install Terraform and Terragrunt.
+2. Install AWS CLI
+2. Use `aws configure` to set AWS CLI credentials.
+3. Navigate to the Terraform directory, adjust `terragrunt.hcl` for your state store and modify terraform.tfvars to use the s3 name you wish for, then execute:
+   - `terragrunt init`
+   - `terragrunt plan`
+   - `terragrunt apply`
+
+#### Running Python Script Locally
+
+1. Ensure Python is installed.
+2. In the `python` folder, run `pip install -r requirements.txt`.
+3. Set `BUCKET_NAME` and `CLOUDFRONT_DOMAIN` as environment variables or modify the script.
+4. Execute the script with `python assignment-code.py`.
+
+## Conclusion
+
+This guide outlines the deployment process for both automated and manual setups, utilizing GitHub Actions, Terraform, Terragrunt, and Python for a comprehensive DevOps cycle.
--- python/assignment-code.py
+++ python/assignment-code.py
@@ -3,33 +3,51 @@ import boto3
 import os
 import json
 
-# Constants
+# Constants defining the URL for the JSON data, the filename for filtered products,
+# and environment variables for AWS S3 bucket name and CloudFront domain.
 JSON_URL = 'https://dummyjson.com/products'
 FILE_NAME = 'filtered_products.json'
-S3_BUCKET_NAME = os.environ.get('BUCKET_NAME', 'bucket-name')
-CLOUDFRONT_DOMAIN = os.environ.get('CLOUDFRONT_DOMAIN', 'cloudfront-domain')
+S3_BUCKET_NAME = os.environ.get('BUCKET_NAME', 'default-bucket-name')
+CLOUDFRONT_DOMAIN = os.environ.get('CLOUDFRONT_DOMAIN', 'default-cloudfront-domain')
 
-# A. Download JSON
+"""
+Script Overview:
+This script automates the process of downloading a JSON file from a specified URL,
+filtering the data based on certain criteria (e.g., product price), saving the filtered data
+to a new JSON file, uploading this file to an AWS S3 bucket, and finally accessing
+the file through a CloudFront distribution.
+
+Steps:
+A. Download JSON data from a given URL.
+B. Filter the JSON data based on a specified condition.
+C. Upload the filtered JSON data to an S3 bucket.
+D. Attempt to download the uploaded file via CloudFront and print the data.
+"""
+
+# Download JSON data from the specified URL.
 response = requests.get(JSON_URL)
 data = response.json()
 
-# B. Parse JSON and filter products
+# Parse the JSON data and filter products based on the specified condition.
+# Here, we're filtering for products with a price of $100 or more.
 filtered_products = [product for product in data['products'] if product['price'] >= 100]
 
-# Save to new JSON file
+# Save the filtered list of products to a new JSON file.
 with open(FILE_NAME, 'w') as file:
     json.dump(filtered_products, file)
 
-# C. Upload to S3
+# Upload the filtered JSON file to the specified S3 bucket.
 s3_client = boto3.client('s3')
 s3_client.upload_file(FILE_NAME, S3_BUCKET_NAME, FILE_NAME)
 
-# D. Download the JSON file via CloudFront
+# Download the JSON file via CloudFront using the constructed URL.
+# This demonstrates accessing the uploaded content via CloudFront, potentially for public access.
 cloudfront_url = f'https://{CLOUDFRONT_DOMAIN}/{FILE_NAME}'
 response = requests.get(cloudfront_url)
 
 if response.ok:
-    # Print the entire filtered JSON data
+    # If the file is successfully downloaded via CloudFront, print the filtered data.
     print(json.dumps(filtered_products, indent=4))
 else:
+    # If there's an issue accessing the file via CloudFront, indicate failure.
     print("Failed to download JSON via CloudFront.")
